7a372d5a976214b0efa8e8e85f716c68a78f4afb
Dominic <dominicchild09@yahoo.com>
2025-02-21 19:53:49 +0000

    implemented Child model and Receipt model. Also linked forms and receipts.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -4,13 +4,15 @@ const schema = a.schema({
     User: a.model({
         userID: a.id().required(), // Unique ID from Tenant Login
         email: a.string().required(), // Email from Tenant Login
-        name: a.string().required(), // Users Name
-        permissionGroup: a.enum(["ADMIN","MANAGER","SOCIAL_WORKER"]), // User role
+        firstName: a.string().required(), // Users Name
+        lastName: a.string().required(),
+        permissionGroup: a.enum(["ADMIN","MANAGER","SOCIAL_WORKER"]), // User role (SUBJECT TO CHANGE)
         lastLogin: a.datetime(), // Timestamp of last login
     }).authorization(allow => [allow.owner()]),
 
 
     Form: a.model({
+        formID: a.id().required(),
         caseNumber: a.string(),
         reason: a.string(),
         amount: a.float(),
@@ -23,11 +25,35 @@ const schema = a.schema({
         addressLineTwo: a.string(),
         addessTown: a.string(),
         addressPostcode: a.string(),
+        receipt: a.hasOne('Receipt','formID')
     }).authorization(allow => [allow.owner()]),
 
     Todo: a.model({
       content: a.string(),
     }).authorization((allow) => [allow.owner()]),
+
+
+    // Child model from the Persons table in the SQL Schema
+    Child: a.model({
+      childId: a.id().required(),
+      firstName: a.string().required(),
+      lastName: a.string().required(),
+      dateOfBirth: a.date().required(),
+      sex: a.string().required(),
+      gender: a.string().required()
+    }),
+
+    Receipt: a.model({
+      recepitID: a.id().required(),
+      formID: a.id(),
+      form: a.belongsTo('Form', 'formID'),
+      transactionDate: a.date(),
+      merchantName: a.string(),
+      paymentMethod: a.string(),
+      subtotal: a.float(),
+      itemDesc: a.string(),
+      whoPurchased: a.belongsTo('User', 'userID')
+    })
 });
 
 export type Schema = ClientSchema<typeof schema>
@@ -41,29 +67,3 @@ export const data = defineData({
         },
     },
 });
-
-/*
-can interact with data models using the generated GraphQL API.
-Example: Fetching Users
-query ListUsers {
-  listUsers {
-    items {
-      userId
-      email
-    }
-  }
-}
-
-
-Creating a New User
-
-mutation CreateForm {
-  createForm(input: {
-    userID: "unique-id",
-    email: "user email",
-    name: "users name",
-    permissionGroup: "SOCIAL_WORKER"
-  }) 
-}
-
-*/
