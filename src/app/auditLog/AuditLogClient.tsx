44b1ea10ec5e4c46423085661c4418ae1a1add9c
your_username <riazwaan03@gmail.com>
2025-02-23 17:20:19 +0000

    feat: custom date&time display format

--- src/app/auditLog/AuditLogClient.tsx
+++ src/app/auditLog/AuditLogClient.tsx
@@ -7,6 +7,22 @@ interface AuditLogClientProps {
   logs: AuditLogEntry[];
 }
 
+// Function to display date&time in custom format
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false, // 24-hour time
+  };
+  
+  return new Intl.DateTimeFormat("en-GB", options)
+    .format(date)
+    .replace(",", "");
+}
+
 export default function AuditLogClient({ logs }: AuditLogClientProps) {
   return (
     <table className="govuk-table">
@@ -14,7 +30,7 @@ export default function AuditLogClient({ logs }: AuditLogClientProps) {
       <thead className="govuk-table__head">
         <tr className="govuk-table__row">
           <th scope="col" className="govuk-table__header">Event</th>
-          <th scope="col" className="govuk-table__header">Date</th>
+          <th scope="col" className="govuk-table__header">Date&Time</th>
         </tr>
       </thead>
       <tbody className="govuk-table__body">
@@ -22,7 +38,7 @@ export default function AuditLogClient({ logs }: AuditLogClientProps) {
           logs.map((log, index) => (
             <tr key={index} className="govuk-table__row">
               <th scope="row" className="govuk-table__header">{log.action}</th>
-              <td className="govuk-table__cell">{log.date.toLocaleString()}</td>
+              <td className="govuk-table__cell">{formatDate(log.date)}</td>
             </tr>
           ))
         ) : (
