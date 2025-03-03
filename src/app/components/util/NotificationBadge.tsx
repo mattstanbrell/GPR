04b68941d5c00e1fa957af40cb965e0e79fab3df
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-03 20:22:35 +0000

    remove unnecessary async

--- src/app/components/util/NotificationBadge.tsx
+++ src/app/components/util/NotificationBadge.tsx
@@ -3,7 +3,7 @@ interface NotificationBadgeProps {
     className?: String
 }
 
-const NotificationBadge = async ({count, className}: NotificationBadgeProps) => {
+const NotificationBadge = ({count, className}: NotificationBadgeProps) => {
   return (
     <div className={"bg-(--color-button-primary) text-white rounded-[50%] w-6 h-6 flex items-center justify-center " + className}>
       {(count > 9) ? '9+' : count.toString()}
