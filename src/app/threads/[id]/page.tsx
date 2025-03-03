422500187e2db6b8b8da6e4f6ba907af48ed8fda
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-03 15:20:16 +0000

    feat: thread pages and urls

---
+++ src/app/threads/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Thread = async () => {
+  return (
+    <div>
+      <h1>Thread</h1>
+    </div>
+  )
+}
+
+export default Thread
---
+++ src/app/threads/page.tsx
@@ -0,0 +1,9 @@
+const AllThreads = async () => {
+  return (
+    <div>
+      <h1>AllThreads</h1>
+    </div>
+  )
+}
+
+export default AllThreads
