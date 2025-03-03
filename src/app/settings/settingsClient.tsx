2b4dc46293afde74a6753fe3adb3514a3103b9bd
your_username <riazwaan03@gmail.com>
2025-03-03 17:03:54 +0000

    feat: implement functions to update settings locally

--- src/app/settings/settingsClient.tsx
+++ src/app/settings/settingsClient.tsx
@@ -6,16 +6,28 @@ import * as React from 'react';
 //   fontColour: string;
 // }
 
-type UserSettingsProps = {fontSize: number, fontColour: string}
+type UserSettingsProps = {fontSize: number, fontColour: string, bgColour: string, spacing: number}
 // type UserSettingsProps = Promise<{userSettings: {fontSize: number, fontColour: string}}>
 
 export default function SettingsClient( props: {userSettings : UserSettingsProps }) {
 
   const [fontSize, setFontSize] = React.useState(props.userSettings.fontSize);
-  setFontSize(2)
-  console.log(fontSize)
+  const [fontColour, setFontColour] = React.useState(props.userSettings.fontColour);
+  const [bgColour, setBgColour] = React.useState(props.userSettings.bgColour);
+  const [spacing, setSpacing] = React.useState(props.userSettings.spacing);
+  //console.log(fontSize, fontColour, bgColour, spacing)
+  
+  const handleFontSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedFontSize = parseInt(event.target.value);
+    setFontSize(selectedFontSize); // Call setFontSize with the selected value
+    console.log(`Font size set to: ${selectedFontSize}`); // Optional: Log the value to the console
+  };
 
-  // const handleFontSizeChange = (event: Event, newValue: number | number[]) => {
+  const handleSettingsChange = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(fontSize, fontColour, bgColour, spacing);
+  };
+  // const handleSettingsChange = (event: Event, newSettings: 'fontSize' | 'fontColor' | 'bgColor' | 'spacing') => {
   //   setFontSize(newValue as number);
   // };
   
@@ -34,18 +46,18 @@ export default function SettingsClient( props: {userSettings : UserSettingsProps
         >
           This page allows you to adapt our website to suit your needs.
           The settings you choose on this page will be saved for future visits.
-          Should you wish to return to the standard settings, use the "Reset" button.
+          Should you wish to return to the standard settings, use the &quot;Reset&quot; button.
         </p>
-        <form>
+        <form onSubmit={handleSettingsChange}>
           <h3 className="govuk-heading-m">Choose your text preferences</h3>
           <div className="govuk-form-group">
             <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} className="form__control">
               <label className="govuk-label">Font size</label>
-              <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue="standard">
-                <option value="1x">Standard (1x)</option>
-                <option value="2x">Large (2x)</option>
-                <option value="3x">Extra Large (3x)</option>
-                <option value="4x">Ultra Large (4x)</option>
+              <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue={1} onChange={handleFontSizeChange}>
+                <option value={1}>Standard (1x)</option>
+                <option value={2}>Large (2x)</option>
+                <option value={3}>Extra Large (3x)</option>
+                <option value={4}>Ultra Large (4x)</option>
               </select>
             </div>
             <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
@@ -62,10 +74,10 @@ export default function SettingsClient( props: {userSettings : UserSettingsProps
             <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} className="form__control">
               <label className="govuk-label">Letter spacing</label>
               <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue="standard">
-                <option value="0">Standard (0)</option>
-                <option value="+1">+1</option>
-                <option value="+2">+2</option>
-                <option value="+3">+3</option>
+                <option value={0}>Standard (0)</option>
+                <option value={1}>+1</option>
+                <option value={2}>+2</option>
+                <option value={3}>+3</option>
               </select>
             </div>
             <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
@@ -91,7 +103,7 @@ export default function SettingsClient( props: {userSettings : UserSettingsProps
               </div>
             </fieldset>
             <div style={{ width: "100%", height: ".25rem", backgroundColor: "#f0e8f0", marginTop: "25px", marginBottom: "15px" }}></div>
-            <div className="setting-buttons" style={{display: 'flex', justifyContent: 'space-evenly'}}>
+            <div className="setting-buttons">
               <button type="submit" className="settings-button--reset" data-module="govuk-button">
                 Reset to default
               </button>
@@ -106,18 +118,16 @@ export default function SettingsClient( props: {userSettings : UserSettingsProps
             <div>
               <h3 className="govuk-heading-m">Preview</h3>
               <div>
-                <p className="govuk-body-s">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam vestibulum velit eu nunc. Nullam adipiscing condimentum augue.
-                </p>
-                <p className="govuk-body-s">
-                  Praesent tellus velit, ultricies sed, ornare eu, consectetuer sit amet, felis. Sed mollis vestibulum mauris. Nunc a tortor vitae nibh faucibus interdum.
-                </p>
-                <p className="govuk-body-s">
-                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce vitae eros. Cras interdum.
-                </p>
-                <p className="govuk-body-s">
-                  Vivamus quam nunc, consequat quis, volutpat non, venenatis sed, nisi. In pede. Cras ut nulla. Etiam scelerisque, est at aliquet suscipit, augue ipsum euismod justo, vitae pellentesque nulla erat nec tellus. Sed a dui sit amet pede tempor semper. In hac habitasse platea dictumst.
-                </p>
+              <p className="govuk-body-s" style={{float: 'left'}}>
+                <img src={'/preview_image.gif'} alt="Preview image" style={{float: 'right'}} />
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam vestibulum velit eu nunc. Nullam adipiscing condimentum augue.
+                <br />
+                Praesent tellus velit, ultricies sed, ornare eu, consectetuer sit amet, felis. Sed mollis vestibulum mauris. Nunc a tortor vitae nibh faucibus interdum.
+                <br />
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce vitae eros. Cras interdum.
+                <br />
+                Vivamus quam nunc, consequat quis, volutpat non, venenatis sed, nisi. In pede. Cras ut nulla. Etiam scelerisque, est at aliquet suscipit, augue ipsum euismod justo, vitae pellentesque nulla erat nec tellus. Sed a dui sit amet pede tempor semper. In hac habitasse platea dictumst.
+              </p>
               </div>
             </div>
           </div>
