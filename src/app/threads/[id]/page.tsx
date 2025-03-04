c404e0f100063cc3b3d88a1acff41dc263dc2b6e
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-04 17:45:10 +0000

    feat: default thread and sidebar view

--- src/app/threads/[id]/page.tsx
+++ src/app/threads/[id]/page.tsx
@@ -1,11 +1,15 @@
+import Thread from "@/app/components/threads/Thread"
 import ThreadsSidebar from "@/app/components/threads/ThreadsSidebar"
 
-const Thread = async () => {
+const ThreadPage = async ({params} : {params: Promise<{ id: string }>}) => {
+  const { id } = await params;
+
   return (
-    <div>
-      <ThreadsSidebar/>
+    <div className="flex flex-row">
+      <ThreadsSidebar selectedId={id} className="shrink-1 w-1/3 preset-secondary" />
+      <Thread threadId={id} className="flex-1 grow border-2" />
     </div>
   )
 }
 
-export default Thread
+export default ThreadPage
