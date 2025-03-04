1a50adbad9fca10c70bdea372ca06d64ea246f3a
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-04 17:50:33 +0000

    feat: displaying a thread
    Example thread has been placed as an example for now.

--- src/app/components/threads/Thread.tsx
+++ src/app/components/threads/Thread.tsx
@@ -1,11 +1,47 @@
+interface ThreadProps {
+    threadId?: string
+    className?: string
+    isMobile?: boolean
+}
 
+const thread = {
+    name: "Bob",
+    threadId: "1",
+    users: ["Alice", "Bob"],
+    formId: "1",
+    messages: [
+        {
+            sender: "Bob",
+            message: "I'm unsure why that didn't work",
+            timestamp: "2021-09-01T12:00:00Z"
+        },
+        {
+            sender: "Alice",
+            message: "I think you need to add more detail to the requirements",
+            timestamp: "2021-09-01T12:01:00Z"
+        }
+    ]
+}
 
-const Thread = () => {
-  return (
-    <div>
-      <h1>Thread</h1>
-    </div>
-  )
+const Thread = ({ threadId, className, isMobile }: ThreadProps) => {
+    return (
+        <div className={`flex flex-col ${className}`}>
+            
+            <div className="bg-(--color-background-light) pt-7 ps-7">
+                {isMobile ? <p className="app-alt-text">text</p> : null}
+                {threadId?
+                    <h1>{thread.name}</h1>:
+                    <h1>No thread selected</h1>
+                }
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                Input
+            </div>
+        </div>
+    )
 }
 
 export default Thread
