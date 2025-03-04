abf4e28a3b86dc56e17fce1019cc37e8346e6b40
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-04 23:23:59 +0000

    refactor: move logic into threads container

--- src/app/threads/[id]/page.tsx
+++ src/app/threads/[id]/page.tsx
@@ -1,14 +1,10 @@
-import Thread from "@/app/components/threads/Thread"
-import ThreadsSidebar from "@/app/components/threads/ThreadsSidebar"
+import ThreadsContainer from "@/app/components/threads/ThreadsContainer";
 
 const ThreadPage = async ({params} : {params: Promise<{ id: string }>}) => {
   const { id } = await params;
 
   return (
-    <div className="flex flex-row">
-      <ThreadsSidebar selectedId={id} className="shrink-1 w-1/3 preset-secondary" />
-      <Thread threadId={id} className="flex-1 grow border-2" />
-    </div>
+    <ThreadsContainer threadId={id} />
   )
 }
 
--- src/app/threads/page.tsx
+++ src/app/threads/page.tsx
@@ -1,12 +1,8 @@
-import Thread from "@/app/components/threads/Thread"
-import ThreadsSidebar from "@/app/components/threads/ThreadsSidebar"
+import ThreadsContainer from "@/app/components/threads/ThreadsContainer";
 
 const ThreadPage = async () => {
   return (
-    <div className="flex flex-row">
-      <ThreadsSidebar className="shrink-1 w-1/3 preset-secondary" />
-      <Thread className="flex-1 grow border-2" />
-    </div>
+    <ThreadsContainer />
   )
 }
 
