d3cc6845936cef0c5c47e1dad2842492cc47c1e3
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-04 23:23:33 +0000

    feat: responsive threads container
    Aim of this component is to display a Whatsapp style messaging UI, where on smaller screens (phone) the user can swap between seeing all threads and an open thread. Meanwhile on larger screens both are visible at once

--- src/app/components/threads/ThreadsContainer.tsx
+++ src/app/components/threads/ThreadsContainer.tsx
@@ -1,4 +1,7 @@
-import ThreadRow from "./ThreadRow"
+'use client';
+import { useEffect, useState } from "react";
+import ThreadsSidebar from "./ThreadsSidebar";
+import Thread from "./Thread";
 
 
 const threads = [
@@ -12,6 +15,7 @@ const threads = [
         name: "James",
         threadId: "2",
         unreadCount: 0,
+        message: "I think we need to add more detail to the requirements"
     },
     {
         name: "Dave",
@@ -21,31 +25,29 @@ const threads = [
     }
 ]
 
-const ThreadsContainer = async () => {
-    return (
-        <div>
-            <h1>All Threads</h1>
-            <table className="flex flex-col border-0">
-                <thead>
-                    <tr className="flex flex-row text-xl bg-(--color-secondary) px-5 p-2 text-(--color-text-light) ">
-                        <th className="flex-1/3  text-left">Name</th>
-                        <th className="flex-2/3 text-left">Last Message</th>
-                        <th className="shrink-0 text-right">Unread</th>
-                    </tr>
-                </thead>
-                <tbody className="flex flex-col">
-                    {threads.map((thread) => (
-                                <ThreadRow 
-                                    name={thread.name} 
-                                    threadId={thread.threadId} 
-                                    message={thread.message} 
-                                    unreadCount={thread.unreadCount} 
-                                    key={thread.threadId}
-                                    className={"border-t-1 p-2 px-5 hover:!bg-(--color-background-medium) focus:outline-(--color-accent) active:!bg-(--color-background-dark)"}/>
+interface ThreadsContainerProps {
+    threadId?: string
+}
 
-                    ))}
-                </tbody>
-            </table>
+const ThreadsContainer = ({threadId} : ThreadsContainerProps) => {
+    const [isMobile, setIsMobile] = useState(false);
+    const [toggleScreen, setToggleScreen] = useState(false); // if isMobile is true: true will show the thread, false will show the threads 
+
+    useEffect(() => {
+        const mediumWindowSize = 768;
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < mediumWindowSize);
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <div className="flex flex-row">
+            <ThreadsSidebar threads={threads} selectedId={threadId} isMobile={isMobile} className={`${!isMobile? " w-1/3" : "w-full" } shrink-1 preset-secondary`} />
+            {isMobile? null : <Thread threadId={threadId} isMobile={isMobile} className="flex-1 grow border-2" />}
         </div>
     )
 }
