5e963e22f64eb172cbe8668eb3d2f1e79e5c1102
Dean Whitbread <dean_whitbread@hotmail.com>
2025-03-06 18:00:05 +0000

    style: Change font wrap to none for mobile.

--- src/app/components/navigation/FullscreenMenu.tsx
+++ src/app/components/navigation/FullscreenMenu.tsx
@@ -9,7 +9,7 @@ const FullscreenMenu = ({handleToggle, handleClick} : {handleToggle: () => void,
 
     return (
         <div className="size-full p-5 bg-[var(--hounslow-primary)] flex justify-center text-white font-bold text-3xl"> 
-            <table className="w-[60%] mt-[8vh] max-h-3/5 min-h-2/5 text-white">
+            <table className="w-[60%] mt-[8vh] max-h-3/5 min-h-2/5 text-white text-nowrap">
                 <tbody>
                     {menuItems.map((item, index) => (
                         <tr key={ index } onClick={handleToggle}>
