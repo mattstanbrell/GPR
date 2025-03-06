fff8b6a7c6cd107e10e4083abd12491afd4d8bce
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-06 11:54:17 +0000

    feat: screen toggle

--- src/app/components/threads/ThreadsSidebar.tsx
+++ src/app/components/threads/ThreadsSidebar.tsx
@@ -1,26 +1,27 @@
 import ThreadRow from "./ThreadRow"
-import Image from "next/image"
+import Toggle from "./Toggle"
 
 
 interface ThreadsSidebarProps {
     threads: ThreadType[]
+    sidebarToggle: () => void
     className?: string
     selectedId?: string
     isMobile?: boolean
+
 }
 
-const ThreadsSidebar = ({className, threads, selectedId, isMobile} : ThreadsSidebarProps) => {
+const ThreadsSidebar = ({className, sidebarToggle, threads, selectedId, isMobile} : ThreadsSidebarProps) => {
     return (
         <div className={`flex flex-col ${className}`}>
             <div className="relative font-bold flex-1 min-h-20 text-3xl w-full app-keep app-background justify-self-center content-center">
                 <p className="justify-self-center app-background">All Threads</p>
-                {isMobile ? <Image 
-                    src="/more-options.svg" 
-                    alt="More Options" 
-                    width={30} 
-                    height={30}
-                    className="absolute right-2 bottom-6 filter-(--color-background-lightest-filter)"
-                /> : null}
+                {isMobile ? 
+                    <Toggle
+                        sidebarToggle={sidebarToggle}
+                        className="absolute right-2 top-6 filter-(--color-background-lightest-filter)"
+                    /> : null
+                }
             </div>
             <table className="flex flex-col border-0">
                 <tbody className="flex flex-col">
