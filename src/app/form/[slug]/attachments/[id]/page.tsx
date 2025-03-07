bad98d07dfecee96a368d4548af335c95bfd715b
Dean Whitbread <dean_whitbread@hotmail.com>
2025-03-07 18:51:30 +0000

    style: Format the form into a table.

--- src/app/form/[slug]/attachments/[id]/page.tsx
+++ src/app/form/[slug]/attachments/[id]/page.tsx
@@ -2,53 +2,37 @@
 
 import React, { useState } from "react";
 import { useFormStatus } from "react-dom";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const getReceiptData = () => {
     return {
-        "total": 6.83,
+        "total": 3.06,
         "items": [
-            { "name": "CKN BURGERS", "quantity": 1, "cost": 0.97 },
+            { "name": "CKN BURGERS", "quantity": 1, "cost": 1.97 },
             { "name": "SARDINES", "quantity": 1, "cost": 0.34 },
-            { "name": "PORRIDGE OATS", "quantity": 1, "cost": 0.75 },
-            { "name": "PEAR PACK", "quantity": 1, "cost": 0.49 },
-            { "name": "TIN TOMATOES", "quantity": 1, "cost": 0.35 },
-            { "name": "WHOLEMEAL", "quantity": 1, "cost": 0.45 },
-            { "name": "S/BERRY JAM", "quantity": 1, "cost": 0.39 },
-            { "name": "VEGETABLES", "quantity": 1, "cost": 0.49 },
-            { "name": "PASSATA", "quantity": 1, "cost": 0.35 },
-            { "name": "KIDNEY BEANS", "quantity": 1, "cost": 0.30 },
-            { "name": "JACKET POTATO", "quantity": 1, "cost": 0.49 },
-            { "name": "ONION", "quantity": 1, "cost": 0.59 },
-            { "name": "MOZZARELLA", "quantity": 1, "cost": 0.47 },
-            { "name": "SPAGHETTI", "quantity": 2, "cost": 0.40 }
+            { "name": "PORRIDGE OATS", "quantity": 2, "cost": 0.75 }
         ]
     };
-}
-
-const Submit = () => {
-    const { pending } = useFormStatus();
-    return <button type="submit">{ !(pending) ? "Submit" : "Submitting" }</button>
 }
 
 const Upload = () => {
     const router = useRouter();
     const [receiptData, setReceiptData] = useState(getReceiptData());
-    const attachmentId = useParams().id
+    const { pending } = useFormStatus();
 
     // retrieve from S3 bucket
     const name = "hotel_for_jim.jpg";
 
     // will need to become a server function for async/await
-    const handleFormSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleFormSubmission = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const data = {
             total: parseFloat(formData.get("total") as string) || 0,
             items: receiptData.items.map((_, index) => ({
                 name: formData.get(`items[${index}].name`),
-                quantity: parseInt(formData.get(`items[${index}.quantity]`) as string) || 0,
-                cost: parseFloat(formData.get(`items[${index}.cost]`) as string) || 0,
+                quantity: parseInt(formData.get(`items[${index}].quantity`) as string) || 0,
+                cost: parseFloat(formData.get(`items[${index}].cost`) as string) || 0,
             }))
         }
 
@@ -61,22 +45,31 @@ const Upload = () => {
         <>
             <h2>{ name }</h2>
             <form onSubmit={(event) => handleFormSubmission(event)} >
-                <div>
-                    <label>Total £
-                        <input name="total" type="number" defaultValue={ receiptData ? receiptData.total : 0.00 } />    
-                    </label>
-                </div>
-                <div className="flex-3">
-                    Item Name No. Cost £
-                    {receiptData && receiptData.items && receiptData.items.map(({ name, quantity, cost}, index) => (
-                        <div key={ index }>
-                                <input name={`items[${index}].name`} type="text" defaultValue={ name ? name : "" } />
-                                <input name={`items[${index}].quantity`} type="number" defaultValue={ quantity ? quantity : 0 } min="0" />
-                                <input name={`items[${index}].cost`} type="number" defaultValue={ cost ? cost : 0.00 } min="0.00" step="0.01" />
-                        </div>
-                    ))}
-                </div>
-                <Submit />
+                <table>
+                    <tbody>
+                        <tr>
+                            <td colSpan={2} className="text-right">
+                                Total £
+                            </td>
+                            <td><input name="total" type="number" defaultValue={ receiptData ? receiptData.total : 0.00 } />   </td>
+                        </tr>
+                        <tr>
+                            <td>Item Name</td>
+                            <td>No.</td>
+                            <td>Cost £</td>
+                        </tr>
+                        {receiptData && receiptData.items && receiptData.items.map(({ name, quantity, cost}, index) => (
+                            <tr key={ index }>
+                                    <td><input name={`items[${index}].name`} type="text" defaultValue={ name ? name : "" } /></td>
+                                    <td><input name={`items[${index}].quantity`} type="number" defaultValue={ quantity ? quantity : 0 } min="0" /></td>
+                                    <td><input name={`items[${index}].cost`} type="number" defaultValue={ cost ? cost : 0.00 } min="0.00" step="0.01" /></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <button type="submit" className="hover:cursor-pointer">
+                    { !(pending) ? "Submit" : "Submitting" }
+                </button>
             </form>
         </>
     )
