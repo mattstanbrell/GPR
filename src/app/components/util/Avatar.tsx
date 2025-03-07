250519e96eec6827e23ad5492632a4c0ef93c556
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-07 18:02:57 +0000

    feat: responsive tooltip
    Can be reused on multiple components. For now being used on the avatar component

--- src/app/components/util/Avatar.tsx
+++ src/app/components/util/Avatar.tsx
@@ -1,8 +1,12 @@
+import { Tooltip } from "./Tooltip"
+
 interface AvatarProps {
     text: string
     colour: string
     className?: string
     style?: React.CSSProperties
+    onClick?: () => void
+    tooltipText?: string
 }
 
 // Grab Initials from name
@@ -11,10 +15,16 @@ export function getInitials(name: string){
     return name.split(" ").map((word) => word[0]).join("").slice(0, 2).toUpperCase()
 }
 
-export const Avatar = ({ text, colour, className, style }: AvatarProps) => {
+export const Avatar = ({ text, colour, className, style, onClick, tooltipText }: AvatarProps) => {
     return (
-        <div style={style} className={`rounded-[50%] flex items-center justify-center font-bold ${colour} ${className}`}>
-            <p className="text-white text-[2em] font-bold  leading-none">{text}</p>
-        </div>
+        <Tooltip content={tooltipText}>
+            <div 
+                style={style} 
+                className={`rounded-[50%] flex items-center justify-center font-bold ${colour} ${className}`}
+                onClick={onClick}
+            >
+                <p className="text-white text-[2em] font-bold  leading-none">{text}</p>
+            </div>
+        </Tooltip>
     )
 }
