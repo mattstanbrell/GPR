526c567480fdac431bd03ef2113472cb419cc01e
Pratt-Gyrotron <pratap.a.guha@gmail.com>
2025-03-07 15:56:42 +0000

    Implement more testing infrastructure

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,15 +2,15 @@
 // import SocialWorkerButtons from "../components/dashboard/SocialWorkerButtons";
 // import ManagerButtons from "../components/dashboard/ManagerButtons";
 // import AdminButtons from "../components/dashboard/AdminButtons";
-
-
+//
+//
 // const exampleUser = {
 // 	firstName: "John",
 // 	permissionGroup: "admin"
 // }
-
-
-
+//
+//
+//
 // function renderButtons(permissionGroup: string) {
 // 	switch (permissionGroup) {
 // 		case 'socialworker':
@@ -23,7 +23,7 @@
 // 			return <div>This is the default home page, you should not be here</div>
 // 	}
 // }
-
+//
 // const Home = async () => {
 // 	const user = exampleUser;
 // 	if (!user) {
@@ -36,24 +36,44 @@
 // 		</div>
 // 	)
 // }
-
-
+//
+//
 // export default Home;
 
 //src/app/page.tsx
 
+import CreateThreadForm from "../components/createThreadForm";
+import ThreadList from "../components/threadList";
 import CreateUserForm from "../components/createUserForm";
-import UserList from "../components/userList"
-import CreateChildForm from "../components/createChildForm";
-import ChildList from "../components/childList"
+import UserList from "../components/userList";
+import CreateUserThreadForm from "../components/createUserThreadForm";
+import UserThreadList from "../components/userThreadList";
+import CreateFormForm from "../components/createFormForm";
+import FormList from "../components/formList";
+import CreateFormAssigneeForm from "../components/createFormAssigneeForm";
+import FormAssigneeList from "../components/formAssigneeList";
+import CreateMessageForm from "../components/createMessageForm";
+import MessageList from "../components/messageList";
+import UserMessageList from "../components/userMessageList";
+import ConstantTest from "../components/constantTest";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<CreateUserForm/>
 			<UserList/>
-			<CreateChildForm/>
-			<ChildList/>
+			<CreateMessageForm/>
+			<MessageList/>
+			<CreateThreadForm/>
+			<ThreadList/>
+			<CreateUserThreadForm/>
+			<UserThreadList/>
+			<CreateFormForm/>
+			<FormList/>
+			<CreateFormAssigneeForm/>
+			<FormAssigneeList/>
+			<UserMessageList/>
+			<ConstantTest/>
 		</main>
 	);
 }
