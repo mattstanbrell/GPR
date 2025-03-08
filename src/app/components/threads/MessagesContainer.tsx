48fecccf3950f6509b11d6dc573be6ad89fd7a51
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-08 02:03:27 +0000

    feat: user message colour
    Messages from the user appear in a different colour

--- src/app/components/threads/MessagesContainer.tsx
+++ src/app/components/threads/MessagesContainer.tsx
@@ -1,3 +1,4 @@
+import { AuthGetCurrentUserServer } from "@/utils/amplifyServerUtils";
 import Message from "./Message"
 import { MessageType } from "./types"
 
@@ -5,27 +6,27 @@ interface MessagesContainerProps {
     messages: MessageType[]
 }
 
-const currentUser = {
-    name: "Alice",
-    userID: "1",
-    threads: ["1", "2", "3"]
-}
 
 
-const MessagesContainer = ({messages} : MessagesContainerProps) => {
-  return (
-    <div>
-        {messages.map((message, i) => {
-            return (
-                <Message 
-                    message={message} 
-                    className={`flex flex-col ${currentUser.userID == message.userID} ${i == messages.length - 1 ? "mb-4" : "mb-2"}`} 
-                    key={message.id}
-                />
-            )
-        })}
-    </div>
-  )
+const MessagesContainer = async ({ messages }: MessagesContainerProps) => {
+    const currentUser = await AuthGetCurrentUserServer();
+
+    return (
+        <div>
+            {messages.map((message, i) => {
+                return (
+                    <Message
+                        message={message}
+                        className={
+                            `flex flex-col 
+                            ${currentUser?.userId == message.userID ? "bg-(--colour-background-medium)" : "bg-(--colour-background-dark)"} 
+                            ${i == messages.length - 1 ? "mb-4" : "mb-2"}`}
+                        key={message.id}
+                    />
+                )
+            })}
+        </div>
+    )
 }
 
 export default MessagesContainer
