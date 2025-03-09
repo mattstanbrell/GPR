c12fbf0159eb10d07d772ab784952a154f4e79c9
Dean Whitbread <dean_whitbread@hotmail.com>
2025-03-09 20:50:38 +0000

    fix: rename components in Formboard.tsx.

--- src/app/components/formboard/Formboard.tsx
+++ src/app/components/formboard/Formboard.tsx
@@ -3,9 +3,9 @@
 
 import Image from 'next/image';
 import FormboardTable from "@/app/components/formboard/FormboardTable";
-import { type User } from '@/app/types/models';
+import { type Form } from '@/app/types/models';
 
-const FormboardHeader = ({boardTitle, handleIndex} : {boardTitle: string, handleIndex: (isIncrement: boolean) => void}) => {
+const Header = ({boardTitle, handleIndex} : {boardTitle: string, handleIndex: (isIncrement: boolean) => void}) => {
     return (
         <div className="w-full p-2 flex  text-center font-bold text-xl">
             <div className='md:w-0 md:invisible xs:sm:visible cursor-pointer' onClick={() => handleIndex(false)}>
@@ -25,7 +25,7 @@ const NavigationButton = ({src, alt} : {src: string, alt: string}) => {
     )
 }
 
-const FormboardTable = ({boardTitle, boardForms} : {boardTitle: string, boardForms: Array<User>}) => {
+const Table = ({boardTitle, boardForms} : {boardTitle: string, boardForms: Array<Form>}) => {
     return (
         <div className="h-[90%] p-3 bg-[var(--color-background-light)] drop-shadow-[0_4px_4px__rgba(0,0,0,0.75)] rounded-[16px]"> 
             <FormboardTable boardTitle={ boardTitle } boardForms={ boardForms }/>
@@ -36,8 +36,8 @@ const FormboardTable = ({boardTitle, boardForms} : {boardTitle: string, boardFor
 const Formboard = ({ boardTitle, boardForms, handleIndex }: { boardTitle: string, boardForms: Array<any>, handleIndex: (isIncrement: boolean) => void }) => {
     return (
         <>
-            <FormboardHeader boardTitle={ boardTitle } handleIndex={ handleIndex } />
-            <FormboardTable boardTitle={ boardTitle } boardForms={ boardForms }
+            <Header boardTitle={ boardTitle } handleIndex={ handleIndex } />
+            <Table boardTitle={ boardTitle } boardForms={ boardForms } />
         </>
     )
 }
