a440dc3289cc4289b9be2cc9a6739f940fda4f95
Dean Whitbread <dean_whitbread@hotmail.com>
2025-03-09 19:42:04 +0000

    refactor: change functions in Formboard.tsx to React components.

--- src/app/components/formboard/Formboard.tsx
+++ src/app/components/formboard/Formboard.tsx
@@ -3,29 +3,29 @@
 
 import Image from 'next/image';
 import FormboardTable from "@/app/components/formboard/FormboardTable";
-import { type Schema } from '../../../../amplify/data/resource'
+import { type User } from '@/app/types/models';
 
-const getFormboardHeader = (boardTitle: string, handleIndex: (isIncrement: boolean) => void ) => {
+const FormboardHeader = ({boardTitle, handleIndex} : {boardTitle: string, handleIndex: (isIncrement: boolean) => void}) => {
     return (
         <div className="w-full p-2 flex  text-center font-bold text-xl">
             <div className='md:w-0 md:invisible xs:sm:visible cursor-pointer' onClick={() => handleIndex(false)}>
-                { getNavigationButton("/previous.svg", "picture of a circle with a left arrow") }
+                <NavigationButton src="/previous.svg" alt="picture of a circle with a left arrow" />
             </div>
             <h2 className='w-full text-[var(--color-background-dark)]'>{ boardTitle }</h2>
             <div className='md:invisible xs:sm:visible md:w-0 cursor-pointer' onClick={() => handleIndex(true)}>
-                { getNavigationButton("/next.svg", "picture of a circle with a right arrow") }
+                <NavigationButton src="/next.svg" alt="picture of a circle with a right arrow" />
             </div>
         </div>
     )
 }
 
-const getNavigationButton = (src: string, alt: string) => {
+const NavigationButton = ({src, alt} : {src: string, alt: string}) => {
     return (
         <Image src={ src } alt={ alt } width={ 40 } height={ 40 } style={{ filter: "var(--hounslow-primary-filter)" }}/>
     )
 }
 
-const getFormboardTable = (boardTitle: string, boardForms: Array<any>) => {
+const FormboardTable = ({boardTitle, boardForms} : {boardTitle: string, boardForms: Array<User>}) => {
     return (
         <div className="h-[90%] p-3 bg-[var(--color-background-light)] drop-shadow-[0_4px_4px__rgba(0,0,0,0.75)] rounded-[16px]"> 
             <FormboardTable boardTitle={ boardTitle } boardForms={ boardForms }/>
@@ -36,8 +36,8 @@ const getFormboardTable = (boardTitle: string, boardForms: Array<any>) => {
 const Formboard = ({ boardTitle, boardForms, handleIndex }: { boardTitle: string, boardForms: Array<any>, handleIndex: (isIncrement: boolean) => void }) => {
     return (
         <>
-            { getFormboardHeader(boardTitle, handleIndex) }
-            { getFormboardTable(boardTitle, boardForms) }
+            <FormboardHeader boardTitle={ boardTitle } handleIndex={ handleIndex } />
+            <FormboardTable boardTitle={ boardTitle } boardForms={ boardForms }
         </>
     )
 }
