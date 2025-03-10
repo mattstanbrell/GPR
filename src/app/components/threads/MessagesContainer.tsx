696e063d7b237fb4058b12b3992ca07e29438348
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-10 05:54:52 +0000

    fix: look of user submitted messages

--- src/app/components/threads/MessagesContainer.tsx
+++ src/app/components/threads/MessagesContainer.tsx
@@ -14,15 +14,18 @@ const MessagesContainer = ({ messages }: MessagesContainerProps) => {
     const currentUser = useAuth();
 
     return (
-        <div>
+        <div className="flex flex-col flex-1 gap-5 max-h-100 p-4 overflow-y-auto">
             {messages?.map((message, i) => {
                 return (
                     <Message
                         message={message}
                         className={
-                            `flex flex-col 
-                            ${currentUser?.id == message.id ? "bg-(--colour-background-medium)" : "bg-(--colour-background-dark)"} 
-                            ${i == messages.length - 1 ? "mb-4" : "mb-2"}`}
+                            `flex flex-col
+                            text-white
+                            ${currentUser?.id == message.id ? 
+                                "bg-(--color-background-medium) self-end" : 
+                                "bg-(--color-background-dark)"} 
+                            ${i == messages.length - 1 ? "mb-4" : null}`}
                         key={message.id}
                     />
                 )
