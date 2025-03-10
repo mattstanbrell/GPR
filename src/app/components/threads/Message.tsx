e1ff1c2d09a682be582053df167d9f3a190421f8
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-10 05:54:30 +0000

    clean up look of messages

--- src/app/components/threads/Message.tsx
+++ src/app/components/threads/Message.tsx
@@ -1,5 +1,6 @@
 import { getName } from "@/utils/authHelpers"
 import { MessageType } from "./types"
+import { formatTimestamp } from "../util/_helpers"
 
 interface MessageProps {
     message: MessageType
@@ -9,10 +10,13 @@ interface MessageProps {
 
 const Message = ({ message, className }: MessageProps) => {
     return (
-        <div className={`${className}`}>
-            <p>{getName(message.user)}</p>
+        <div className={`w-2/3 rounded-xl p-3 pt-2 gap-1 ${className}`}>
+            <div className="flex gap-2  items-center">
+                <div className="font-bold">{getName(message.user)}</div>
+                <div className="text-xs  text-(--color-text-light-alt)">{formatTimestamp(new Date(message.timeSent))}</div>
+            </div>
             <p>{message.content}</p>
-            <p>{message.timeSent}</p>
+            
         </div>
     )
 }
