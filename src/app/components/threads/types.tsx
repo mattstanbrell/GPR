76e4d76ab47ff0ce26ccdef66678dc2177cf02fd
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-10 20:05:58 +0000

    fix: type discrepancies

--- src/app/components/threads/types.tsx
+++ src/app/components/threads/types.tsx
@@ -3,16 +3,32 @@ import { Schema } from "../../../../amplify/data/resource";
 export type ThreadType = {
     id: string
     messages: Schema["Thread"]["type"]["messages"]
+    name?: string
     lastMessage?: Schema["Message"]["type"]
     unreadCount?: number | null
+    users: Schema["Thread"]["type"]["users"]
+    form: Schema["Thread"]["type"]["form"]
 }
 
-type UserType = Schema["User"]["type"];
+export type UserType = {
+    id: string
+    firstName: string
+    lastName: string
+};
+
+export type FormType = {
+    id: string
+};
+
+export type UserThreadType = Schema["UserThread"]["type"];
+export type ThreadUsers = UserThreadType[];
+export type Message = Schema["Message"]["type"];
+export type ThreadMessages = Message[];
 
 
 export type MessageType = {
     id: string, 
     user: Schema["Message"]["type"]["user"], 
     content: string, 
-    timeSent: string
+    timeSent?: Schema["Message"]["type"]["timeSent"]
 }; 
