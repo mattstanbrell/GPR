c3d25321b0a9eb57c17385fbcb150c18b5761b3c
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-11 18:31:06 +0000

    feat: loading

--- src/app/components/threads/ThreadRow.tsx
+++ src/app/components/threads/ThreadRow.tsx
@@ -4,15 +4,25 @@ import NotificationBadge from "../util/NotificationBadge"
 import Link from "next/link"
 
 interface ButtonProps {
-    name: string
-    threadId: string
+    loading?: boolean
+    name?: string
+    threadId?: string
     message?: string
     unreadCount?: number
     className?: string
     selected?: boolean
 }
 
-const ThreadRow = ({ name, threadId, message, unreadCount, className, selected }: ButtonProps) => {
+const ThreadRow = ({ name, threadId, message, unreadCount, className, selected, loading }: ButtonProps) => {
+    if (loading) {
+        return (
+            <tr className="flex  font-bold cursor-pointer bg-gray-400 animate-pulse">
+                <td className="flex-1 h-40  ">
+                </td>
+            </tr>
+        )
+    }
+
     return (
         <tr
             key={threadId}
@@ -27,18 +37,19 @@ const ThreadRow = ({ name, threadId, message, unreadCount, className, selected }
                 className="
                         w-full
                         flex-1
-                        app-hoverable p-2 px-7  
+                        app-hoverable 
                         app-keep 
                         app-background
                         app-hoverable
             ">
-                <Link href={`${THREAD}${threadId}`}  className={`gap-3 flex ${selected?"preset-light" : "preset-secondary"} shrink items-center`}>
-                    <p className="text-2xl flex-1 truncate text-left" title={name}>{name}</p>
-                    <div className={` w-1/2 flex-1 flex gap-1 app-alt-text`}>
-                        <div className="flex-1 text-left truncate" title={message}>{message}</div>
+                <Link href={`${THREAD}${threadId}`}  className={`p-2 px-5  gap-2 flex ${selected?"preset-light" : "preset-secondary"} shrink items-center`}>
+                    <p className="text-xl flex-1 truncate text-left" title={name}>{name}</p>
+                    <div className={` ${message? "md:w-10 lg:w-1/2 w-1/2 md:flex-none flex-1" : "flex-none"}  flex gap-2 app-alt-text justify-end`}>
+                        <div className="flex-1 text-left truncate md:hidden lg:block" title={message}>{message}</div>
                         <div className="">
-                            {unreadCount &&
-                                <NotificationBadge count={unreadCount} />
+                            {unreadCount ?
+                                ((unreadCount > 0) &&
+                                <NotificationBadge count={unreadCount} />) : null
                             }
                         </div>
                     </div>
