d445466a501e831ff6abe22df11986da379e77c0
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-11 18:32:11 +0000

    move fetching to layout

--- src/app/components/threads/ThreadsContainer.tsx
+++ src/app/components/threads/ThreadsContainer.tsx
@@ -1,62 +1,59 @@
 'use client';
-import { useEffect, useState } from "react";
+
+import { useState } from "react";
 import ThreadsSidebar from "./ThreadsSidebar";
 import Thread from "./Thread";
-import { getThreadbyID, getThreadsWithUser } from "@/utils/apis";
 import { ThreadType } from "./types";
 import useIsMobileWindowSize from "@/utils/responsivenessHelpers";
 import { useUserModel } from "@/utils/authenticationUtils";
+import { PrimaryButton } from "../util/Button";
+import { seed, displayBackend, deleteModels } from "./dummy";
 
 
 interface ThreadsContainerProps {
-    threadId?: string
+    thread?: ThreadType  | null
+    threads?: ThreadType[] | null
+    loadingThreads?: boolean
+    loadingThread?: boolean
     startWithSidebar ?: boolean
 }
 
-const ThreadsContainer = ({threadId, startWithSidebar = true} : ThreadsContainerProps) => {
+const ThreadsContainer = ({thread, threads, loadingThread, loadingThreads, startWithSidebar = true} : ThreadsContainerProps) => {
     const isMobile = useIsMobileWindowSize();
-    const [threads, setThreads] = useState<ThreadType[] | null>(null);
-    const [currentThread, setCurrentThread] = useState<ThreadType | null>(null);
     const [viewSidebar, setViewSidebar] = useState(startWithSidebar); //When the screen is mobile, the sidebar is hidden by default
     const currentUser = useUserModel();
-    
-    useEffect(() => {
-        async function fetchThreads() {
-            if(!currentUser) return;
-            if(!threadId) return;
 
-            try{
-                const [threads, thread] = await Promise.all([
-                    getThreadsWithUser(currentUser.id),
-                    getThreadbyID(threadId)
-                ]);
-
-                setThreads(threads);
-                setCurrentThread(thread);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchThreads();
-    }, [currentUser, threadId]);
 
     return (
-        <div className="flex flex-row">
-            { (!isMobile || viewSidebar) &&
-                <ThreadsSidebar 
-                    threads={threads}
-                    sidebarToggle={() => setViewSidebar(!viewSidebar)}
-                    selectedId={threadId} 
-                    isMobile={isMobile} 
-                    className={`${!isMobile? " w-1/3" : "w-full" } shrink-1 preset-secondary`} />
-            }
-            { !(isMobile && viewSidebar) &&
-                <Thread 
-                    thread={currentThread}
-                    sidebarToggle={() => setViewSidebar(!viewSidebar)} 
-                    isMobile={isMobile} 
-                    className="flex-1 grow" />
-            }
+        <div className="flex flex-col h-full">
+            
+            <div className="flex flex-row">
+                
+                { (!isMobile || viewSidebar) &&
+                    <ThreadsSidebar 
+                        threads={threads}
+                        sidebarToggle={() => setViewSidebar(!viewSidebar)}
+                        selectedId={thread?.id} 
+                        isMobile={isMobile}
+                        loading={loadingThreads}
+                        className={`${!isMobile? " w-1/3" : "w-full" } shrink-1 preset-secondary`} />
+                }
+                { !(isMobile && viewSidebar) &&
+                    <Thread 
+                        thread={thread}
+                        sidebarToggle={() => setViewSidebar(!viewSidebar)} 
+                        isMobile={isMobile} 
+                        loading={loadingThread}
+                        className="flex-1 grow" />
+                }
+            </div>
+            {/* { currentUser &&          
+                    <div className="flex flex-row h-10 justify-center">
+                        <PrimaryButton onClick={() => seed(currentUser)} />
+                        <PrimaryButton onClick={() => displayBackend()} />
+                        <PrimaryButton onClick={() => deleteModels(currentUser)} />
+                    </div>
+            } */}
         </div>
     )
 }
