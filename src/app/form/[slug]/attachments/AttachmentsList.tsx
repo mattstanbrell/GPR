baac4373c9357a15c57d809d65048e0453c40c69
your_username <riazwaan03@gmail.com>
2025-03-13 13:50:51 +0000

    feat: add attachments page

---
+++ src/app/form/[slug]/attachments/AttachmentsList.tsx
@@ -0,0 +1,33 @@
+import AttachmentItem from "./AttachmentItem";
+
+interface AttachmentsListProps {
+  files: File[];
+  onDelete: (fileName: string) => void;
+  onUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AttachmentsList = ({ files, onDelete, onUpload }: AttachmentsListProps) => {
+  return (
+    <div className="govuk-form-group">
+      <dl className="govuk-summary-list govuk-summary-list--long-key">
+
+        {files.map((file) => (
+          <AttachmentItem key={file.name} file={file} onDelete={onDelete} />
+        ))}
+
+      </dl>
+
+      <div className="govuk-form-group">
+        <label className="govuk-label" htmlFor="file-upload">
+        Upload receipts (last receipt uploaded shown below)
+        </label>
+        <div className="govuk-drop-zone" data-module="govuk-file-upload">
+          <input className="govuk-file-upload" id="file-upload" type="file" multiple onChange={onUpload}/>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default AttachmentsList;
