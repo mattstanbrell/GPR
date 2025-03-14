379fe05bab7b37f79500b18f5dd0d4e7e7d72f58
your_username <riazwaan03@gmail.com>
2025-03-14 13:46:45 +0000

    feat: settings are now fetched from userId dynamically and saves

--- src/app/settings/SettingsClient.tsx
+++ src/app/settings/SettingsClient.tsx
@@ -13,7 +13,6 @@ const DEFAULT_SPACING = 0;
 
 //add crumbs!!!
 
-// type UserSettingsProps = Promise<{userSettings: {fontSize: number, fontColour: string}}>
 type UserSettings = {
   fontSize: number | null;
   font: string | null;
@@ -24,17 +23,28 @@ type UserSettings = {
 
 export default function SettingsClient() {
   const [loaded, setLoaded] = useState(false);
-  const [currentUserSettings, setCurrentUserSettings] = useState<UserSettings | null>();
+  const [userSettings, setUserSettings] = useState<UserSettings | null>();
+
+  const [fontSize, setFontSize] = useState(DEFAULT_FONT_SIZE);
+  const [font, setFont] = useState(DEFAULT_FONT)
+  const [spacing, setSpacing] = useState(DEFAULT_SPACING);
+  const [fontColour, setFontColour] = useState(DEFAULT_FONT_COLOUR);
+  const [bgColour, setBgColour] = useState(DEFAULT_BG_COLOUR);
+
+  const [tempFontSize, setTempFontSize] = useState(DEFAULT_FONT_SIZE);
+  const [tempFont, setTempFont] = useState(DEFAULT_FONT);
+  const [tempSpacing, setTempSpacing] = useState(DEFAULT_SPACING);
+  const [tempFontColour, setTempFontColour] = useState(DEFAULT_FONT_COLOUR);
+  const [tempBgColour, setTempBgColour] = useState(DEFAULT_BG_COLOUR);
 
   const userModel = useUserModel();
-  console.log("user settings",currentUserSettings)
 
   useEffect(() => {
     const fetchUserSettings = async () => {
       try {
         if (userModel?.id) {
           const userSettings = await getUserSettingsByUserId(userModel.id);
-          setCurrentUserSettings(userSettings);
+          setUserSettings(userSettings);
         }
       } catch (error) {
         console.error("Failed to fetch user settings:", error);
@@ -45,34 +55,44 @@ export default function SettingsClient() {
     fetchUserSettings();
   }, [userModel]);
 
-  const [fontSize, setFontSize] = useState(currentUserSettings?.fontSize ?? DEFAULT_FONT_SIZE);
-  const [font, setFont] = useState(currentUserSettings?.font ?? DEFAULT_FONT)
-  const [spacing, setSpacing] = useState(currentUserSettings?.spacing ?? DEFAULT_SPACING);
-  const [fontColour, setFontColour] = useState(currentUserSettings?.fontColour ?? DEFAULT_FONT_COLOUR);
-  const [bgColour, setBgColour] = useState(currentUserSettings?.bgColour ?? DEFAULT_BG_COLOUR);
-
-  const [tempFontSize, setTempFontSize] = useState(currentUserSettings?.fontSize ?? DEFAULT_FONT_SIZE);
-  const [tempFont, setTempFont] = useState(currentUserSettings?.font ?? DEFAULT_FONT);
-  const [tempSpacing, setTempSpacing] = useState(currentUserSettings?.spacing ?? DEFAULT_SPACING);
-  const [tempFontColour, setTempFontColour] = useState(currentUserSettings?.fontColour ?? DEFAULT_FONT_COLOUR);
-  const [tempBgColour, setTempBgColour] = useState(currentUserSettings?.bgColour ?? DEFAULT_BG_COLOUR);
+  useEffect(() => {
+    const setUserSettings = async () => {
+      try {
+        if (userSettings) {
+          setFontSize(userSettings.fontSize ?? DEFAULT_FONT_SIZE);
+          setFont(userSettings.font ?? DEFAULT_FONT);
+          setSpacing(userSettings.spacing ?? DEFAULT_SPACING);
+          setFontColour(userSettings.fontColour ?? DEFAULT_FONT_COLOUR);
+          setBgColour(userSettings.bgColour ?? DEFAULT_BG_COLOUR);
+          
+          setTempFontSize(userSettings.fontSize ?? DEFAULT_FONT_SIZE);
+          setTempFont(userSettings.font ?? DEFAULT_FONT);
+          setTempSpacing(userSettings.spacing ?? DEFAULT_SPACING);
+          setTempFontColour(userSettings.fontColour ?? DEFAULT_FONT_COLOUR);
+          setTempBgColour(userSettings.bgColour ?? DEFAULT_BG_COLOUR);
+        }
+      } catch (error) {
+        console.error("Failed to set user settings:", error);
+      } finally {
+        setLoaded(true);
+      }
+    };
+    setUserSettings();
+  }, [userSettings]);
 
   const handleFontSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedFontSize = parseFloat(event.target.value);
     setTempFontSize(selectedFontSize);
-    console.log(`Font size set to: ${selectedFontSize}`);
   };
 
   const handleFontChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedFont = event.target.value;
     setTempFont(selectedFont);
-    console.log(`Font set to: ${selectedFont}`);
   };
 
   const handleSpacingChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedSpacing = parseInt(event.target.value);
     setTempSpacing(selectedSpacing);
-    console.log(`Spacing set to: ${selectedSpacing}`);
   };
 
   const handleColourChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -81,7 +101,6 @@ export default function SettingsClient() {
     const selectedBgColour = selectedColours.bgColour;
     setTempFontColour(selectedFontColour)
     setTempBgColour(selectedBgColour);
-    console.log(`Bg colour set to: ${selectedBgColour} and Font colour set to: ${selectedFontColour}`);
   };
 
   const updateSettings = (fontSize: number, font: string, spacing: number, fontColour: string, bgColour: string) => {
@@ -152,7 +171,7 @@ export default function SettingsClient() {
           <div className="govuk-form-group">
             <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} className="form__control">
               <label className="govuk-label">Font size</label>
-              <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue={1} onChange={handleFontSizeChange}>
+              <select style={{width: "min(700px, 60%)"}} className="govuk-select" value={tempFontSize} onChange={handleFontSizeChange}>
                 <option value={1}>1x (Standard)</option>
                 <option value={1.5}>1.5x</option>
                 <option value={2}>2x</option>
@@ -162,7 +181,7 @@ export default function SettingsClient() {
             <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
             <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} className="form__control">
               <label className="govuk-label">Font</label>
-              <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue="lexend" onChange={handleFontChange}>
+              <select style={{width: "min(700px, 60%)"}} className="govuk-select" value={tempFont} onChange={handleFontChange}>
                 <option value="lexend">Lexend (Standard)</option>
                 <option value="times">Times</option>
                 <option value="courier">Courier</option>
@@ -172,7 +191,7 @@ export default function SettingsClient() {
             <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
             <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} className="form__control">
               <label className="govuk-label">Letter spacing</label>
-              <select style={{width: "min(700px, 60%)"}} className="govuk-select" defaultValue={0} onChange={handleSpacingChange}>
+              <select style={{width: "min(700px, 60%)"}} className="govuk-select" value={tempSpacing} onChange={handleSpacingChange}>
                 <option value={0}>0 (Standard)</option>
                 <option value={1}>+1</option>
                 <option value={2}>+2</option>
@@ -180,27 +199,63 @@ export default function SettingsClient() {
               </select>
             </div>
             <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
+
+
             <fieldset className="govuk-fieldset">
-              <h3 className="govuk-heading-m">Choose your colour preferences</h3>
+              <legend className="govuk-heading-m">Choose your colour preferences</legend>
               <div className="govuk-radios" data-module="govuk-radios">
-                <label className="form__label--radio" style={{backgroundColor: '#f1f0f0'}}>
-                  <input onChange={handleColourChange} value='{"fontColour": "#000000", "bgColour": "#FFFFFF"}' className="form__radio" name="color-pref" type="radio" defaultChecked style={{width: '1.1875rem', height: '1.1875rem'}}/>
+                <label className="form__label--radio" style={{ backgroundColor: '#f1f0f0' }}>
+                  <input
+                    onChange={handleColourChange}
+                    value='{"fontColour": "#000000", "bgColour": "#FFFFFF"}'
+                    className="form__radio"
+                    name="color-pref"
+                    type="radio"
+                    checked={tempFontColour === "#000000" && tempBgColour === "#FFFFFF"}
+                    style={{ width: '1.1875rem', height: '1.1875rem' }}
+                  />
                   Standard
                 </label>
-                <label className="form__label--radio" style={{backgroundColor: '#000',color: '#ff0'}}>
-                  <input onChange={handleColourChange} value='{"fontColour": "#FF0", "bgColour": "#000"}' className="form__radio" name="color-pref" type="radio" style={{width: '1.1875rem', height: '1.1875rem'}}/>
+                <label className="form__label--radio" style={{ backgroundColor: '#000', color: '#ff0' }}>
+                  <input
+                    onChange={handleColourChange}
+                    value='{"fontColour": "#FF0", "bgColour": "#000"}'
+                    className="form__radio"
+                    name="color-pref"
+                    type="radio"
+                    checked={tempFontColour === "#FF0" && tempBgColour === "#000"}
+                    style={{ width: '1.1875rem', height: '1.1875rem' }}
+                  />
                   High contrast
                 </label>
-                <label className="form__label--radio" style={{backgroundColor: '#fff9d2'}}>
-                  <input onChange={handleColourChange} value='{"fontColour": "#000000", "bgColour": "#fff9d2"}' className="form__radio" name="color-pref" type="radio" style={{width: '1.1875rem', height: '1.1875rem'}}/>
+                <label className="form__label--radio" style={{ backgroundColor: '#fff9d2' }}>
+                  <input
+                    onChange={handleColourChange}
+                    value='{"fontColour": "#000000", "bgColour": "#fff9d2"}'
+                    className="form__radio"
+                    name="color-pref"
+                    type="radio"
+                    checked={tempFontColour === "#000000" && tempBgColour === "#fff9d2"}
+                    style={{ width: '1.1875rem', height: '1.1875rem' }}
+                  />
                   Cream
                 </label>
-                <label className="form__label--radio" style={{backgroundColor: '#9fcfff'}}>
-                  <input onChange={handleColourChange} value='{"fontColour": "#000000", "bgColour": "#9fcfff"}' className="form__radio" name="color-pref" type="radio" style={{width: '1.1875rem', height: '1.1875rem'}}/>
+                <label className="form__label--radio" style={{ backgroundColor: '#9fcfff' }}>
+                  <input
+                    onChange={handleColourChange}
+                    value='{"fontColour": "#000000", "bgColour": "#9fcfff"}'
+                    className="form__radio"
+                    name="color-pref"
+                    type="radio"
+                    checked={tempFontColour === "#000000" && tempBgColour === "#9fcfff"}
+                    style={{ width: '1.1875rem', height: '1.1875rem' }}
+                  />
                   Blue
                 </label>
               </div>
             </fieldset>
+
+
             <div style={{ width: "100%", height: ".25rem", backgroundColor: "#f0e8f0", marginTop: "25px", marginBottom: "15px" }}></div>
             <div className="setting-buttons">
               <button type="submit" value="reset" className="settings-button--reset" data-module="govuk-button">
