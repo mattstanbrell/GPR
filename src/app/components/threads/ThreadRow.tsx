c2c7df8824e40a7592dfbb35ad1454d5d65ce20a
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-16 10:29:40 +0000

    refactor: use one threads link

--- src/app/components/threads/ThreadRow.tsx
+++ src/app/components/threads/ThreadRow.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { THREAD } from "@/app/constants/urls"
+import { THREADS } from "@/app/constants/urls"
 import NotificationBadge from "../util/NotificationBadge"
 import Link from "next/link"
 
@@ -42,7 +42,7 @@ const ThreadRow = ({ name, threadId, message, unreadCount, className, selected, 
                         app-background
                         app-hoverable
             ">
-                <Link href={`${THREAD}${threadId}`}  className={`p-2 px-5  gap-2 flex ${selected?"preset-light" : "preset-secondary"} shrink items-center`}>
+                <Link href={`${THREADS}${threadId}`}  className={`p-2 px-5  gap-2 flex ${selected?"preset-light" : "preset-secondary"} shrink items-center`}>
                     <p className="text-xl flex-1 truncate text-left" title={name}>{name}</p>
                     <div className={` ${message? "md:w-10 lg:w-1/2 w-1/2 md:flex-none flex-1" : "flex-none"}  flex gap-2 app-alt-text justify-end`}>
                         <div className="flex-1 text-left truncate md:hidden lg:block" title={message}>{message}</div>
