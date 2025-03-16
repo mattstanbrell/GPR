798640dc1e58c8950d7b02f213761bd3a5a99263
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-16 15:33:38 +0000

    fix: amplify errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 		if (user) {
 			router.push(HOME);
 		}
-	}, [isLoading, router]);
+	}, [isLoading, user, router]);
 
 	if (isLoading) {
 		return <h3 className="text-center">Loading...</h3>;
