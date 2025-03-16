3f369a832238401a14e7014ee631148401916f15
TheLeoYeo <leo.yeofr@gmail.com>
2025-03-16 10:07:41 +0000

    remove dev links

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -34,14 +34,10 @@ export const auth = defineAuth({
 			logoutUrls: [
 				"http://localhost:3000",
 				"https://main.d2sc3b0jj94stq.amplifyapp.com",
-        		"https://dev-dashboards.d2sc3b0jj94stq.amplifyapp.com/",
-				"https://dev-direct-messaging.d2sc3b0jj94stq.amplifyapp.com/",
 			],
 			callbackUrls: [
 				"http://localhost:3000",
 				"https://main.d2sc3b0jj94stq.amplifyapp.com",
-       			"https://dev-dashboards.d2sc3b0jj94stq.amplifyapp.com/",
-				"https://dev-direct-messaging.d2sc3b0jj94stq.amplifyapp.com/",
 			],
 		},
 	},
