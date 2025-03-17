cb0e45d992f0e7bf88e8b09b375ee88c60b6600a
your_username <riazwaan03@gmail.com>
2025-03-17 13:55:45 +0000

    fix: attempt to fix amplify deploy 002

--- src/app/settings/SettingsClient.tsx
+++ src/app/settings/SettingsClient.tsx
@@ -133,62 +133,72 @@ export default function SettingsClient() {
   }
   
   return (
-    <div>
-      <h1 className="govuk-heading-xl" style={{ backgroundColor: '#e5f2eb' }} >Settings</h1>
-      <div>
-        <p className="summary"
-          style={{
-            border: '.25rem solid #f0e8f0',
-            color: '#642f6c',
-            fontSize: '1.125rem',
-            marginBottom: '2.25rem',
-            padding: '1.5rem',
-          }}
-        >
-          This page allows you to adapt our website to suit your needs.
-          The settings you choose on this page will be saved for future visits.
-          Should you wish to return to the standard settings, use the &quot;Reset&quot; button.
-        </p>
-        <form onSubmit={handleSettingsChange}>
-          <h3 className="govuk-heading-m">Choose your text preferences</h3>
-          <div className="govuk-form-group">
+    <>
+      {loaded ? (
+        <div>
+          <h1 className="govuk-heading-xl" style={{ backgroundColor: '#e5f2eb' }} >Settings</h1>
+          <div>
+            <p className="summary"
+              style={{
+                border: '.25rem solid #f0e8f0',
+                color: '#642f6c',
+                fontSize: '1.125rem',
+                marginBottom: '2.25rem',
+                padding: '1.5rem',
+              }}
+            >
+              This page allows you to adapt our website to suit your needs.
+              The settings you choose on this page will be saved for future visits.
+              Should you wish to return to the standard settings, use the &quot;Reset&quot; button.
+            </p>
+            <form onSubmit={handleSettingsChange}>
+              <h3 className="govuk-heading-m">Choose your text preferences</h3>
+              <div className="govuk-form-group">
 
-            <FontSizeSelector fontSize={tempFontSize} updateTempFontSize={setTempFontSize}/>
-            <Seperator/>
+                <FontSizeSelector fontSize={tempFontSize} updateTempFontSize={setTempFontSize}/>
+                <Seperator/>
 
-            <FontSelector font={tempFont} updateTempFont={setTempFont}/>
-            <Seperator/>
-            
-            <SpacingSelector spacing={tempSpacing} updateTempSpacing={setTempSpacing}/>
-            <Seperator/>
+                <FontSelector font={tempFont} updateTempFont={setTempFont}/>
+                <Seperator/>
+                
+                <SpacingSelector spacing={tempSpacing} updateTempSpacing={setTempSpacing}/>
+                <Seperator/>
 
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-heading-m">Choose your colour preferences</legend>
-              <ColourRadioSet 
-                fontColour={tempFontColour} 
-                bgColour={tempBgColour} 
-                updateTempFontColour={setTempFontColour} 
-                updateTempBgColour={setTempBgColour}
-              />
-            </fieldset>
+                <fieldset className="govuk-fieldset">
+                  <legend className="govuk-heading-m">Choose your colour preferences</legend>
+                  <ColourRadioSet 
+                    fontColour={tempFontColour} 
+                    bgColour={tempBgColour} 
+                    updateTempFontColour={setTempFontColour} 
+                    updateTempBgColour={setTempBgColour}
+                  />
+                </fieldset>
 
-            <div style={{ width: "100%", height: ".25rem", backgroundColor: "#f0e8f0", marginTop: "25px", marginBottom: "15px" }}></div>
-            <div className="setting-buttons">
-              <button type="submit" value="reset" className="settings-button--reset" data-module="govuk-button">
-                Reset to default
-              </button>
-              <button type="submit" value="save" className="settings-button--save" data-module="govuk-button">
-                Save and continue
-              </button>
-              <button type="submit" value="preview" className="settings-button--preview" data-module="govuk-button">
-                Preview
-              </button>
-            </div>
-            <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
-            <Preview userSettings={{ fontSize, font, spacing, fontColour, bgColour }}/>
+                <div style={{ width: "100%", height: ".25rem", backgroundColor: "#f0e8f0", marginTop: "25px", marginBottom: "15px" }}></div>
+
+                <div className="setting-buttons">
+                  <button type="submit" value="reset" className="settings-button--reset" data-module="govuk-button">
+                    Reset to default
+                  </button>
+                  <button type="submit" value="save" className="settings-button--save" data-module="govuk-button">
+                    Save and continue
+                  </button>
+                  <button type="submit" value="preview" className="settings-button--preview" data-module="govuk-button">
+                    Preview
+                  </button>
+                </div>
+
+                <div style={{ width: "100%", height: "1px", backgroundColor: "#b1b4b6", marginTop: "15px", marginBottom: "15px" }}></div>
+
+                <Preview userSettings={{ fontSize, font, spacing, fontColour, bgColour }}/>
+
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
-  );
+        </div> 
+      ) : (
+        <h3>Loading Logs...</h3>
+      )}
+    </>
+  )
 }
