bcc63b352209f5c72939779d9c06bde17a246d5d
your_username <riazwaan03@gmail.com>
2025-03-17 14:20:49 +0000

    fix: 004

--- src/utils/auditLogUtils.ts
+++ src/utils/auditLogUtils.ts
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState} from "react";
+import { useEffect} from "react";
 import { createAuditLog } from "./apis";
 import { useUserModel } from "./authenticationUtils";
 
