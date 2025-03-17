1627bb8dcf9c1c3fb86e5a594fce7ff4beafccc8
Dean Whitbread <dean_whitbread@hotmail.com>
2025-03-17 14:29:25 +0000

    feat: add ChildTable function.

--- src/app/components/admin/Table.tsx
+++ src/app/components/admin/Table.tsx
@@ -1,17 +1,84 @@
 'use client'
 
+import React from "react"
 import { Child, User, Team } from "@/app/types/models"
+import { redirect } from "next/navigation"
 
-export const Table = ({data} : {data: User[] | Child[] | Team[]}) => {
-    // if there is not data to load do
-        // show no users message
-    // else do 
-        // if data contais team objects do
-            // render team table
-        // else if data contains user objects do
-            // render user table
-        // else if data contains child objects fdo
-            // render child table
+export const UserTable = ({users} : { users: User[]}) => {
+    const url = "/admin/user";
+    console.log(users)
+    // build header 
+    const header = (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
+        </thead>
+    )
 
-    return <table></table>
+    // build body
+    const body = (
+        <tbody>
+            { users.map(({id, firstName, lastName, email, permissionGroup}, index) => (
+                <tr key={ index } onClick={ () => redirect(`${url}?id=${id}`) }>
+                    <td>{firstName} {lastName}</td>
+                    <td>{email}</td>
+                    <td>{permissionGroup}</td>
+                </tr>
+            ))}
+        </tbody>
+    )
+
+    // render table with elements
+    return <Table header={ header } body={ body } />
+} 
+
+export const ChildTable = ({children} : { children: Child[]}) => {
+    const url = "/admin/child";
+    // build header 
+    const header = (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>DoB</th>
+                <th>Case Number</th>
+            </tr>
+        </thead>
+    )
+
+    // build body
+    const body = (
+        <tbody>
+            { children.map(({id, caseNumber, firstName, lastName, dateOfBirth}, index) => (
+                <tr key={ index } onClick={ () => redirect(`${url}?id=${id}`) }>
+                    <td>{firstName} {lastName}</td>
+                    <td>{dateOfBirth}</td>
+                    <td>{caseNumber}</td>
+                </tr>
+            ))}
+        </tbody>
+    )
+
+    // render table with elements
+    return <Table header={ header } body={ body } />
+} 
+
+export const TeamTable = ({teams} : { teams: Team[]}) => {
+    // build header 
+
+    // build body
+
+    // render table with elements
+    return <></>
+}  
+
+const Table = ({header, body} : {header: React.ReactElement, body: React.ReactElement}) => {
+    return (
+        <table>
+            { header }
+            { body }
+        </table>
+    )
 }
--- src/app/components/admin/Views.tsx
+++ src/app/components/admin/Views.tsx
@@ -3,7 +3,11 @@
 import { listUsers, listUsersInTeam, listChildren, listTeams } from "@/utils/apis"
 import { useEffect, useState } from "react"
 import { Child, Team, User } from "@/app/types/models"
-import { Table } from "@/app/components/admin/Table"
+import { UserTable, ChildTable, TeamTable } from "@/app/components/admin/Table"
+
+const LoadingMessage = () => {
+    return <h3 className="text-center">Loading...</h3>
+}
 
 export const AllChildrenView = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true); 
@@ -17,7 +21,7 @@ export const AllChildrenView = () => {
         fetchChildren(); 
     }, [])
 
-    return isLoading ? <h3 className="text-center">Loading...</h3> : <Table data={ children } />
+    return isLoading ? <LoadingMessage /> : <ChildTable children={ children } />
 }
 
 export const AllTeamsView = () => {
@@ -32,27 +36,22 @@ export const AllTeamsView = () => {
         fetchTeams(); 
     }, [])
 
-    return isLoading ? <h3 className="text-center">Loading...</h3> : <Table data={ teams } />
+    return isLoading ? <LoadingMessage /> : <TeamTable teams={ teams } />
 }
 
-export const AllUsersView = ({data = []} : {data?: User[]}) => {
+export const AllUsersView = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true); 
-    const [users, setUsers] = useState<User[]>([]); 
+    const [users, setUsers] = useState<User[]>([]);  
+    
+    useEffect(() => {
+        const fetchAllUsers = async () => {
+            setUsers(await listUsers()); 
+            setIsLoading(false); 
+        }
+        fetchAllUsers(); 
+    }, [])
 
-    if (data.length > 0) {
-        setUsers(data);
-        setIsLoading(false); 
-    } else {
-        useEffect(() => {
-            const fetchAllUsers = async () => {
-                setUsers(await listUsers()); 
-                setIsLoading(false); 
-            }
-            fetchAllUsers(); 
-        }, [])
-    }
-
-    return isLoading ? <h3 className="text-center">Loading...</h3> : <Table data={ users } />
+    return isLoading ? <LoadingMessage /> : <UserTable users={ users } />
 }
 
 export const AllTeamMembersView = ({teamId} : {teamId: string}) => {
@@ -67,5 +66,5 @@ export const AllTeamMembersView = ({teamId} : {teamId: string}) => {
         fetchTeamMembers();
     }, [teamId])
 
-    return isLoading ? <h3 className="text-center">Loading...</h3> : <AllUsersView data={ teamMembers } /> 
+    return isLoading ? <LoadingMessage /> : <UserTable users={ teamMembers } /> 
 }
